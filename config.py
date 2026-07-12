02ec56c9cc3c54193f36380ac4b1420717db7400
venkaiahkuncham123-tech <venkaiahkuncham123@gmail.com>
2026-07-12 21:18:17 +0530

    Update database URI to use host.docker.internal

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://root:root@localhost/my_database'
+        'postgresql://root:root@host.docker.internal/my_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
